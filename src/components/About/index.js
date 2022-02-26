b80aa8c33b6b150a89e84953ac8b1d5a2db19851
Carter Supple <Cartersupple123@gmail.com>
2022-02-26 00:36:38 -0800

    added some styling

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import coverImage from '../../assets/profilephoto.jpg';
 function About() {
   return (
-    <section className="my-5">
-      <h1 id="about">Who am I?</h1>
+    <section className="my-5 col">
+      <h1 class ="row" id="about">Who am I?</h1>
       <img src={coverImage} className="my-2" style={{ width: "50%" }} alt="cover" />
       <div className="my-2">
         <p>
